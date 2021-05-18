7c209d4fc5f80501d5cd61e5537f61d664ceebdb
LouisNillus <louis.nillus@gmail.com>
2021-05-18 02:23:45 +0200

    Movement, Bombs & Walls WIP, 2 Players and pseudo collisions

---
+++ Bomberman/Assets/Bomb.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public float explosionTimer;
+    public int range = 1;
+
+    public Bomb(float explosionTimer)
+    {
+        this.explosionTimer = explosionTimer;
+    }
+
+    private void OnEnable()
+    {
+        GridHandler.instance.GetCellFromPos(transform.position).type = EntityType.Bomb;
+        StartCoroutine(Explode());
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator Explode()
+    {
+        yield return new WaitForSeconds(explosionTimer);
+        Boom();
+
+        GridHandler.instance.GetCellFromPos(transform.position).type = EntityType.None;
+        Destroy(this.gameObject);
+    }
+
+    public void Boom()
+    {
+
+    }
+}
--- Bomberman/Assets/GridHandler.cs
+++ Bomberman/Assets/GridHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GridHandler : MonoBehaviour
 {
@@ -12,6 +13,7 @@ public class GridHandler : MonoBehaviour
 
     public Cell[] map;
     public GameObject tilePrefab;
+    public GameObject wallPrefab;
     public int tileResolution;
 
     public Bounds bounds;
@@ -30,7 +32,7 @@ public class GridHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void InitMap()
@@ -42,14 +44,78 @@ public class GridHandler : MonoBehaviour
             for (int j = 0; j < rows; j++)
             {
                 map[(i * lines) + j] = new Cell(new Vector3(j, i, 0) * TileSize());
-                map[(i * lines) + j].go = Instantiate(tilePrefab, map[(i * lines) + j].pos, Quaternion.identity);
+                map[(i * lines) + j].tile = Instantiate(tilePrefab, map[(i * lines) + j].pos, Quaternion.identity);
             }
         }
 
         bounds = GetBounds();
     }
 
+    public Cell GetCellFromPos(Vector3 pos)
+    {
+        foreach (Cell c in map)
+        {
+            if (c.pos == pos) return c;
+        }
 
+        return null;
+    }
+
+    public int GetCellIndexFromPos(Vector3 pos)
+    {
+        foreach (Cell c in map)
+        {
+            if (c.pos == pos) return Array.IndexOf(map, c);
+        }
+
+        Debug.LogError("Can't find any cell index for this position in grid");
+        return map.Length + 1;
+    }
+
+    public Cell GoToNextCell(Vector3 position, Direction direction)
+    {
+        Cell c = null;
+
+        switch (direction)
+        {
+            case Direction.Up:
+                if (GetCellIndexFromPos(position) + rows < map.Length)
+                {
+                    c = map[GetCellIndexFromPos(position) + rows];
+                }
+                break;
+            case Direction.Down:
+                if (GetCellIndexFromPos(position) - rows >= 0)
+                {
+                    c = map[GetCellIndexFromPos(position) - rows];
+                }
+                break;
+            case Direction.Right:
+                if (GetCellIndexFromPos(position) + 1 < map.Length)
+                {
+                    c = map[GetCellIndexFromPos(position) + 1];
+                }
+                break;
+            case Direction.Left:
+                if (GetCellIndexFromPos(position) - 1 >= 0)
+                {
+                    c = map[GetCellIndexFromPos(position) - 1];
+                }
+                break;
+        }
+
+        if (c == null || c.occupied)
+        {
+            map[GetCellIndexFromPos(position)].occupied = true;
+            return map[GetCellIndexFromPos(position)]; //Si on peut pas aller à la suivante on renvoie le case sur laquelle on est
+        }
+        else
+        {
+            c.occupied = true;
+            return c;
+        }
+
+    }
 
     public float TileSize()
     {
@@ -57,7 +123,54 @@ public class GridHandler : MonoBehaviour
     }
     public Bounds GetBounds()
     {
-        return new Bounds((lines-1) * TileSize(), 0, 0, (rows-1) * TileSize());
+        return new Bounds((lines - 1) * TileSize(), 0, 0, (rows - 1) * TileSize());
+    }
+
+    public void SetWall(Cell cell)
+    {
+        Instantiate(wallPrefab, cell.pos, Quaternion.identity);
+        cell.type = EntityType.Wall;
+        cell.occupied = true;
+    }
+
+    public List<Cell> GetAllEmptyCells(bool includeBombs = false)
+    {
+        List<Cell> cells = new List<Cell>();
+
+        if (includeBombs)
+        {
+            foreach(Cell c in map)
+            {
+                if (c.occupied == false) cells.Add(c);
+            }
+        }
+        else
+        {
+            foreach (Cell c in map)
+            {
+                if (c.occupied == false && c.type != EntityType.Bomb) cells.Add(c);
+            }
+        }
+
+        return cells;
+    }
+
+    public List<Cell> CrossCells(Vector3 from, int radius, bool includeSelf = true)
+    {
+        List<Cell> cells = new List<Cell>();
+
+        foreach(Cell c in map)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 8; j++)//TO COMPLETE
+                {
+
+                }
+            }
+        }
+
+        return cells;
     }
 }
 
@@ -65,12 +178,18 @@ public class GridHandler : MonoBehaviour
 public class Cell
 {
     public Vector3 pos = Vector3.zero;
-    public bool test;
-    public GameObject go;
+    public GameObject tile;
+    public EntityType type;
+    public bool occupied = false;
 
     public Cell(Vector3 pos)
     {
         this.pos = pos;
+    }
+
+    public void FreeCell()
+    {
+        occupied = false;
     }
 }
 
@@ -90,3 +209,6 @@ public class Bounds
         this.right = right;
     }
 }
+
+public enum Direction {Up, Down, Left, Right}
+public enum EntityType {None, Bomb, Wall}
