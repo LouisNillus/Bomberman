388122e5b7f74f59a9d2cb3b64a39692b19590a6
Lorenzo <lorenzo.coeugniet@hotmail.com>
2021-05-18 22:31:52 +0200

    Pressure Plate

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -4,8 +4,27 @@ using UnityEngine;
 public class PressurePlate : MonoBehaviour
 {
     [SerializeField] Animator _animator;
-    void OnCollisionEnter(Collision other)
+    Transform _self;
+    bool active = true;
+
+    void Start()
     {
+        _self = transform;
+    }
+
+    void Update()
+    {
+        if (!active) return;
         
+        Cell cell = GridHandler.instance.GetCellFromPos(_self.position);
+        foreach (Player player in GridHandler.instance.players)
+        {
+            if (GridHandler.instance.GetCellFromPos(player.transform.position) == cell)
+            {
+                _animator.Play("PressurePlateOn");
+                
+                break;
+            }
+        }
     }
 }
