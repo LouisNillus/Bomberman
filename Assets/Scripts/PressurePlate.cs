505be0ebd2994f3125f6ea08ce0c4853361b536e
Lorenzo <lorenzo.coeugniet@hotmail.com>
2021-05-18 21:58:19 +0200

    Start pressure plate

---
+++ Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    [SerializeField] Animator _animator;
+    void OnCollisionEnter(Collision other)
+    {
+        
+    }
+}
